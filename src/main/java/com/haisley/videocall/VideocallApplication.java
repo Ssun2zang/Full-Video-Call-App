0c01bae396c0b5819c12901567a1b55ee836a0c3
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-07-24 23:50:00 +0900

    feat: user api

--- src/main/java/com/haisley/videocall/VideocallApplication.java
+++ src/main/java/com/haisley/videocall/VideocallApplication.java
@@ -1,7 +1,11 @@
 package com.haisley.videocall;
 
+import com.haisley.videocall.user.User;
+import com.haisley.videocall.user.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VideocallApplication {
@@ -10,4 +14,31 @@ public class VideocallApplication {
 		SpringApplication.run(VideocallApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			UserService service
+	){
+		return args -> {
+			service.register(User.builder()
+					.username("haisley")
+					.email("haisley@mail.com")
+					.password("aaa")
+					.build());
+			service.register(User.builder()
+					.username("hazel")
+					.email("hazel@mail.com")
+					.password("bbb")
+					.build());
+			service.register(User.builder()
+					.username("noah")
+					.email("noah@mail.com")
+					.password("ccc")
+					.build());
+			service.register(User.builder()
+					.username("hyden")
+					.email("hyden@mail.com")
+					.password("ddd")
+					.build());
+		};
+	}
 }
